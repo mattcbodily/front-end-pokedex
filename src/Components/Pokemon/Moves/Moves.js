6e0d49dbe95bbc38521d662b7293ed1c6de553a8
mattcbodily <mattcbodily@gmail.com>
2020-06-04 16:00:10 -0600

    added routing to about sections

--- src/Components/Pokemon/Moves/Moves.js
+++ src/Components/Pokemon/Moves/Moves.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default props => (
-    <div>About</div>
+    <div>Moves</div>
 )
--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
 import axios from 'axios';
 import Header from '../Header/Header';
+import About from './About/About';
+import Stats from './Stats/Stats';
+import Evolution from './Evolution/Evolution';
+import Moves from './Moves/Moves';
 import './Pokemon.css';
 
 export default props => {
@@ -24,7 +29,18 @@ export default props => {
             <p>{pokemon.name}</p>
             <p>#{pokemon.id}</p>
             <div className='poke-info'>
-
+                <nav className='link-flex'>
+                    <Link to={`/pokemon/${pokemon.id}`} className={`about-links ${props.location.pathname === ('/pokemon/' + pokemon.id) ? 'active-link' : null}`}>About</Link>
+                    <Link to={`/pokemon/${pokemon.id}/stats`} className={`about-links ${props.location.pathname.includes('stats') ? 'active-link' : null}`}>Base Stats</Link>
+                    <Link to={`/pokemon/${pokemon.id}/evolution`} className={`about-links ${props.location.pathname.includes('evolution') ? 'active-link' : null}`}>Evolution</Link>
+                    <Link to={`/pokemon/${pokemon.id}/moves`} className={`about-links ${props.location.pathname.includes('moves') ? 'active-link' : null}`}>Moves</Link>
+                </nav>
+                <Switch>
+                <Route exact path='/pokemon/:id' component={About}/>
+                <Route path='/pokemon/:id/stats' component={Stats}/>
+                <Route path='/pokemon/:id/evolution' component={Evolution}/>
+                <Route path='/pokemon/:id/moves' component={Moves}/>
+                </Switch>
             </div>
         </div>
     )
