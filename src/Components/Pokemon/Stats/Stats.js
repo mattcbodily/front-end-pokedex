eea009eb8e81e8e87e5b6053d2345dcbb8840374
mattcbodily <mattcbodily@gmail.com>
2020-06-05 17:07:15 -0600

    end of day commit

--- src/Components/Pokemon/Stats/Stats.js
+++ src/Components/Pokemon/Stats/Stats.js
@@ -1,13 +1,38 @@
 import React, {useState, useEffect} from 'react';
+import stringWorks from '../../../HOCs/stringWorks';
 import './Stats.css';
 
-export default props => (
-    <div>
-        {props.statsArr?.map((stat, i) => (
-            <section key={i}>
-                <span>{stat.stat.name}</span>
-                <span>{stat.base_stat}</span>
-            </section>
-        ))}
-    </div>
-);
+const Stats = props => {
+    let [formattedStats, setFormattedStats] = useState([]);
+
+    const formatStrings = () => {
+        let format = props.statsArr.map((stat) => {
+            if(stat.stat.name === 'hp'){
+                stat.stat.name = stat.stat.name.toUpperCase();
+            }
+        })
+
+        setFormattedStats(format)
+    }
+
+    useEffect(() => {
+        if(props.statsArr){
+            formatStrings()
+        }
+    }, [props])
+
+    console.log(formattedStats)
+
+    return (
+        <div>
+            {props.statsArr?.map((stat, i) => (
+                <section key={i}>
+                    <p>{stat.stat.name}</p>
+                    <p>{stat.base_stat}</p>
+                </section>
+            ))}
+        </div>
+    )
+}
+
+export default stringWorks(Stats);
