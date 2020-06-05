f40c60533b671e51e7c1f3f52d515a8b8128f48f
mattcbodily <mattcbodily@gmail.com>
2020-06-05 09:46:53 -0600

    About now displays flavor text, as well as height and weight

--- src/Components/Pokemon/About/About.js
+++ src/Components/Pokemon/About/About.js
@@ -1,12 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './About.css';
 
 export default props => {
-    let [aboutInfo, setAboutInfo] = useState('');
+    let [aboutInfo, setAboutInfo] = useState(''),
+        [height, setHeight] = useState(''),
+        [metricHeight, setMetricHeight] = useState(''),
+        [weight, setWeight] = useState(0),
+        [metricWeight, setMetricWeight] = useState(0);
 
-    const getInfo = async() => {
-        const {id} = props.match.params;
+    const getHeight = () => {
+        let totalInches = Math.round(props.height * 3.94),
+            feet = Math.floor(totalInches / 12),
+            remInches = totalInches - (feet * 12);
+
+        let centimeters = props.height * 10,
+            meters;
+
+        if (centimeters >= 100) {
+            meters = centimeters / 100;
+            setMetricHeight(`${meters}m`);
+        } else {
+            setMetricHeight(`${centimeters}cm`);
+        }
+
+        setHeight(`${feet}' ${remInches}"`);
+    }
+
+    const getWeight = () => {
+        let pounds = props.weight / 4.536,
+            trimNum = Number(pounds.toFixed(1));
+
+        let kilograms = props.weight / 10;
+
+        setWeight(trimNum);
+        setMetricWeight(kilograms);
+    }
+
+    const getInfo = async () => {
+        const { id } = props.match.params;
         let flavorText = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
             enFlavorText = flavorText.data.flavor_text_entries.filter((text, i) => text.language.name === 'en');
 
@@ -15,12 +47,25 @@ export default props => {
 
     useEffect(() => {
         getInfo();
-    }, [])
+        getHeight();
+        getWeight();
+    }, [props])
 
-    console.log(props)
-    console.log(aboutInfo)
+
 
     return (
-        <div>About</div>
+        <div className='about'>
+            <p>{aboutInfo}</p>
+            <section className='height-weight'>
+                <section>
+                    <p className='size-prompt'>Height</p>
+                    <p>{height} ({metricHeight})</p>
+                </section>
+                <section>
+                    <p className='size-prompt'>Weight</p>
+                    <p>{weight}lbs ({metricWeight}kg)</p>
+                </section>
+            </section>
+        </div>
     )
 }
