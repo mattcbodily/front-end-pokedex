d41092bf943008767aa72467f94c59f4470f06b0
mattcbodily <mattcbodily@gmail.com>
2020-06-08 15:29:05 -0600

    added styling to Pokemon.js, types now display

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -7,6 +7,8 @@ import About from './About/About';
 import Stats from './Stats/Stats';
 import Evolution from './Evolution/Evolution';
 import Moves from './Moves/Moves';
+import dots from '../../assets/dots.svg';
+import pokeball from '../../assets/pokeball.svg';
 import './Pokemon.css';
 
 const Pokemon = props => {
@@ -17,6 +19,7 @@ const Pokemon = props => {
 
         let pokeObj = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
         pokeObj.data.name = props.strWorks.capitalizeFirst(pokeObj.data.name);
+        pokeObj.data.types = pokeObj.data.types.map(element => props.strWorks.capitalizeFirst(element.type.name))
 
         setPokemon(pokeObj.data);
     }
@@ -28,10 +31,19 @@ const Pokemon = props => {
     return (
         <div className='pokemon'>
             <Header />
-            <img src={pokemon.sprites?.front_default} alt={pokemon.name}/>
-            <p>{pokemon.name}</p>
-            <p>#{pokemon.id}</p>
+            <section className='pokemon-intro'>
+                <h1>{pokemon.name} #{pokemon.id}</h1>
+                <div className='types-flex'>
+                    {pokemon.types?.map((type, i) => (
+                        <div key={i} className={'pokemon-intro-type'}>{type}</div>
+                    ))}
+                </div>
+                <div className='square-style'></div>
+                <img src={dots} alt='dots' className='dots'/>
+                <img src={pokeball} alt='Pokeball Icon' className='poke-icon'/>
+            </section>
             <div className='poke-info'>
+                <img src={pokemon.sprites?.front_default} alt={pokemon.name} className='pokemon-about-image'/>
                 <nav className='link-flex'>
                     <Link to={`/pokemon/${pokemon.id}`} className={`about-links ${props.location.pathname === ('/pokemon/' + pokemon.id) ? 'active-link' : null}`}>About</Link>
                     <Link to={`/pokemon/${pokemon.id}/stats`} className={`about-links ${props.location.pathname.includes('stats') ? 'active-link' : null}`}>Base Stats</Link>
