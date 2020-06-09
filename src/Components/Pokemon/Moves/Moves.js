3fc6f653c09dfca22a6fe8daf043fc2741c829d2
mattcbodily <mattcbodily@gmail.com>
2020-06-09 14:32:02 -0600

    moves desktop display complete

--- src/Components/Pokemon/Moves/Moves.js
+++ src/Components/Pokemon/Moves/Moves.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import stringWorks from '../../../HOCs/stringWorks';
 import './Moves.css';
 
@@ -7,15 +7,15 @@ const Moves = props => {
 
     const sortMoves = () => {
         let levelArr = props.moves?.filter(move => move.version_group_details[0].move_learn_method.name === 'level-up')
-                                   .map(e => {
-                                       return {
-                                           name: props.strWorks.removeHyphen(e.move.name),
-                                           level: e.version_group_details[0].level_learned_at
-                                       }
-                                   })
-                                   .sort((a, b) => {
-                                       return a.level - b.level
-                                   });
+            .map(e => {
+                return {
+                    name: props.strWorks.removeHyphen(e.move.name),
+                    level: e.version_group_details[0].level_learned_at
+                }
+            })
+            .sort((a, b) => {
+                return a.level - b.level
+            });
 
         setLevelMoves(levelArr);
     }
@@ -26,12 +26,15 @@ const Moves = props => {
 
     return (
         <div className='moves'>
-            {levelMoves?.map((move, i) => (
-                <div key={i} className='move-container'>
-                    <p>{move.name}</p>
-                    <p>Lvl {move.level}</p>
-                </div>
-            ))}
+            <h1>Moves</h1>
+            <div className='moves-display'>
+                {levelMoves?.map((move, i) => (
+                    <div key={i} className='move-container'>
+                        <p>{move.name}</p>
+                        <p>Lvl {move.level}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
